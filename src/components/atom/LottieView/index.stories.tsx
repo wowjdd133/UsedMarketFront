6f88b46a4e446ee1560e717ff20692f09e547fbe
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2021-06-08 18:28:22 +0900

    2

--- src/components/atom/LottieView/index.stories.tsx
+++ src/components/atom/LottieView/index.stories.tsx
@@ -4,6 +4,18 @@ import { storiesOf } from '@storybook/react-native';
 import LottieView from './index';
 
 storiesOf('LottieView', module)
-    .add('location-on-pin', () => <LottieView name="location-on-pin" />)
-    .add('location-on-pin-medium', () => <LottieView name="location-on-pin" style={{width:'75%'}} />)
-    .add('location-on-pin-small', () => <LottieView name="location-on-pin" style={{width:'50%'}} />)
+    .add('location-on-pin', () => <LottieView name="location-on-pin" style={{
+        lottieStyle: {
+            width: '100%'
+        }
+    }}/>)
+    .add('location-on-pin-medium', () => <LottieView name="location-on-pin" style={{
+        lottieStyle: {
+            width:'75%'
+        }
+    }} />)
+    .add('location-on-pin-small', () => <LottieView name="location-on-pin" style={{
+        lottieStyle: {
+            width:'50%'
+        }
+    }} />)
