cec6abafcb432e1697d0640e4e083e9456e1a8a8
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2021-06-09 21:23:19 +0900

    [Add, Change] user-profile png, change storyLoader

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -5,14 +5,22 @@
 
 function loadStories() {
   require('../src/components/atom/Button/index.stories');
+  require('../src/components/atom/Icon/index.stories');
   require('../src/components/atom/LottieView/index.stories');
   require('../src/components/atom/Text/index.stories');
+  require('../src/components/molecules/IconButton/index.stories');
+  require('../src/components/organism/Intro/index.stories');
+  require('../src/components/template/Intro/index.stories');
 }
 
 const stories = [
   '../src/components/atom/Button/index.stories',
+  '../src/components/atom/Icon/index.stories',
   '../src/components/atom/LottieView/index.stories',
   '../src/components/atom/Text/index.stories',
+  '../src/components/molecules/IconButton/index.stories',
+  '../src/components/organism/Intro/index.stories',
+  '../src/components/template/Intro/index.stories',
 ];
 
 module.exports = {
