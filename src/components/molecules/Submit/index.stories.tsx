b17c947d939359151449775f5fd31c9d73e3b989
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2021-06-15 11:04:36 +0900

    [Change] Submit add text

--- src/components/molecules/Submit/index.stories.tsx
+++ src/components/molecules/Submit/index.stories.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import SubmitMolecules from './index';
 import { colors, SizeType } from '../../../styles/theme';
+import { Text, View } from 'react-native';
+import TextAtom from '../../atom/Text';
 
 storiesOf('Submit', module)
-    .add('submit', () => <SubmitMolecules
+    .add('submit', () => 
+    <View
+        style={{
+            height: 130,
+            width: '100%',
+        }}
+    >
+<SubmitMolecules
         wrapperStyle={{
-            flex:1,
-            marginHorizontal:15
+            flex: 1,
+            marginHorizontal:15,
         }}
         button={{
             buttonStyle: {
@@ -42,43 +51,115 @@ storiesOf('Submit', module)
                 paddingLeft: 7
             },
         }}
-    />)
-    .add('submit-disabled', () => <SubmitMolecules
-        wrapperStyle={{
-            flex:1,
-            marginHorizontal:15
-        }}
-        button={{
-            buttonStyle: {
-                color: colors.orange,
-                paddingVertical: '10px',
-                borderStyle: {
-                    color: 'transparent',
-                    radius: 4
-                },
-                paddingHorizontal: '6px',
-            },
-            disabled: true,
-            onPress: () => {},
-            text: "인증문자 받기",
-            textStyle: {
-                color: colors.white,
+        text={{
+            text: (
+                <Text>
+                    <TextAtom
+                        style={{
+                            textStyle: {
+                                color: colors.black,
+                                size: SizeType.SMALL
+                            }
+                        }}
+                        text="전화번호가 변경되었나요? "
+                    />
+                    <TextAtom
+                        style={{
+                            textStyle: {
+                                color: colors.black,
+                                size: SizeType.SMALL,
+                                underline: true
+                            },
+                        }}
+                        text="이메일로 계정 찾기"
+                        onPress={() => {}}
+                    />
+                </Text>
+            ),
+            textStyle:{
+                color: colors.black,
                 size: SizeType.SMALL,
-                weight: 'bold',
-                center: true
             },
         }}
-        input={{
-            onChange: (text:string) => {},
-            value:"",
-            placeholder:"휴대폰 번호(- 없이 숫자만 입력)",
-            wrapperStyle: {
-                width:'100%',
-                height: 42,
-                borderWidth: 0.5,
-                borderRadius: 4,
-                borderColor: colors.grey,
-                paddingLeft: 7
-            },
+    />
+    </View>
+    )
+    .add('submit-disabled', () => 
+    <View
+        style={{
+            width: '100%',
+            height: 130
         }}
-    />)
+    >
+        <SubmitMolecules
+                wrapperStyle={{
+                    flex:1,
+                    marginHorizontal:15
+                }}
+                button={{
+                    buttonStyle: {
+                        color: colors.orange,
+                        paddingVertical: '10px',
+                        borderStyle: {
+                            color: 'transparent',
+                            radius: 4
+                        },
+                        paddingHorizontal: '6px',
+                    },
+                    disabled: true,
+                    onPress: () => {},
+                    text: "인증문자 받기",
+                    textStyle: {
+                        color: colors.white,
+                        size: SizeType.SMALL,
+                        weight: 'bold',
+                        center: true
+                    },
+                }}
+                input={{
+                    onChange: (text:string) => {},
+                    value:"",
+                    placeholder:"휴대폰 번호(- 없이 숫자만 입력)",
+                    wrapperStyle: {
+                        width:'100%',
+                        height: 42,
+                        borderWidth: 0.5,
+                        borderRadius: 4,
+                        borderColor: colors.grey,
+                        paddingLeft: 7
+                    },
+                }}
+                text={{
+                    text: (
+                        <Text>
+                            <TextAtom
+                                style={{
+                                    textStyle: {
+                                        color: colors.black,
+                                        size: SizeType.SMALL
+                                    }
+                                }}
+                                text="전화번호가 변경되었나요? "
+                            />
+                            <TextAtom
+                                style={{
+                                    textStyle: {
+                                        color: colors.black,
+                                        size: SizeType.SMALL
+                                    }
+                                }}
+                                text="이메일로 계정 찾기"
+                                onPress={() => {}}
+                            />
+                        </Text>
+                    ),
+                    textStyle:{
+                        color: colors.black,
+                        size: SizeType.SMALL,
+                        underline:true
+                    },
+                    onPress:() => {}
+                }}
+            />
+    </View>
+    )
--- src/components/molecules/Submit/index.tsx
+++ src/components/molecules/Submit/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import WrapperComponent from '../../common/Wrapper';
 import InputAtom, { InputExportStyleType, InputExportType } from '../../atom/Input';
-
 import ButtonAtom, { ButtonExportStyleType, ButtonExportType } from '../../atom/Button/index';
+import TextAtom, { TextExportStyleType, TextExportType } from '../../atom/Text/index';
+
+
 import { StyleProp, ViewStyle } from 'react-native';
 
 interface SubmitMoleculesType {
     input: InputExportStyleType & InputExportType;
     button: ButtonExportType & ButtonExportStyleType
+    text: TextExportType & TextExportStyleType;
     wrapperStyle?: StyleProp<ViewStyle>
 }
 
-const SubmitMolecules = ({input, button, wrapperStyle}:SubmitMoleculesType) => {
+const SubmitMolecules = ({input, button, wrapperStyle, text}:SubmitMoleculesType) => {
     return (
         <WrapperComponent
             style={wrapperStyle ?? {width:'100%'}}
+            isCenter
         >
             <InputAtom
                 {...input}
@@ -27,9 +31,23 @@ const SubmitMolecules = ({input, button, wrapperStyle}:SubmitMoleculesType) => {
                     wrapperStyle: {
                         marginTop: 10,
                         flex: 1,
+                        width:'100%'
                     }
                 }}
             />
+
+            {
+                text ? (
+                    <TextAtom
+                        {...text}
+                        style={{
+                            textStyle: text.textStyle
+                        }}
+                    />
+                ) : null
+            }
+            
+           
         </WrapperComponent>
     )
 }
