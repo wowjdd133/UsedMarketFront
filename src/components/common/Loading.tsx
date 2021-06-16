28a469b35178efe5039bc56d0e0dd01b2e9235aa
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2021-06-17 00:46:10 +0900

    [Fix] loading zIndex 999

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -13,6 +13,7 @@ const LoadingCommon = () => {
                 left: 0,
                 bottom: 0,
                 right: 0,
+                zIndex: 999,
                 justifyContent:'center',
                 alignItems:'center'
             }}
